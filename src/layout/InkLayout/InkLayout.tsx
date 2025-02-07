67e209b3519f680206cdcc33047fdaef48e7179f
prodigyray1125 <vokaraon@gmail.com>
2025-02-07 13:18:02 -0500

    chore: bump to 0.6.1, add missing class

--- src/layout/InkLayout/InkLayout.tsx
+++ src/layout/InkLayout/InkLayout.tsx
@@ -94,7 +94,7 @@ export const InkLayout: React.FC<InkLayoutProps> = ({
               {sideNavigation}
             </div>
           )}
-          <div className="ink:flex-grow ink:flex ink:box-border ink:pb-5">
+          <div className="ink:flex-grow ink:flex ink:box-border ink:pb-5 ink:overflow-hidden">
             {children}
           </div>
         </div>
